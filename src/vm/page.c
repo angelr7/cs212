ecf709f81932439175f6460b5a5168e4cc192d8e
Angel Ruiz <ar7@stanford.edu>
2023-02-21 18:17:27 -0800

    fixed errors

--- src/vm/page.c
+++ src/vm/page.c
@@ -2,24 +2,24 @@
 #include "vm/page.h"
 
 /* Returns a hash value for page p. */
-unsigned page_hash (const struct hash_elem *p_, void *aux UNUSED)
+unsigned
+page_hash(const struct hash_elem *p_, void *aux UNUSED)
 {
-  const struct page *p = hash_entry (p_, struct page, hash_elem);
-  return hash_bytes (&p->virtual_addr, sizeof p->virtual_addr);
+    const struct page *p = hash_entry(p_, struct page, hash_elem);
+    return hash_bytes(&p->virtual_addr, sizeof p->virtual_addr);
 }
 
 /* Returns true if page a precedes page b. */
-bool
-page_less (const struct hash_elem *a_, const struct hash_elem *b_,
-           void *aux UNUSED)
+bool page_less(const struct hash_elem *a_, const struct hash_elem *b_,
+               void *aux UNUSED)
 {
-  const struct page *a = hash_entry (a_, struct page, hash_elem);
-  const struct page *b = hash_entry (b_, struct page, hash_elem);
-  return a->virtual_addr < b->virtual_addr;
+    const struct page *a = hash_entry(a_, struct page, hash_elem);
+    const struct page *b = hash_entry(b_, struct page, hash_elem);
+    return a->virtual_addr < b->virtual_addr;
 }
 
 void init_supplemental_table(struct hash *supplemental_table)
-{   
+{
     hash_init(supplemental_table, page_hash, page_less, NULL);
 }
 
--- src/vm/page.h
+++ src/vm/page.h
@@ -17,6 +17,11 @@ struct page {
     struct thread *process_refrence;
     bool loaded;
     short memory_flag;    
-}
+};
+
+void init_supplemental_table(struct hash*);
+unsigned page_hash(const struct hash_elem *p_, void *aux UNUSED);
+bool page_less(const struct hash_elem *a_, const struct hash_elem *b_,
+               void *aux UNUSED);
 
 #endif  /* vm/page.h */
